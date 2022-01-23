fde021dcb3c1fc3fe03fb60eb8e9d19a29c99074
okaytrev <trevabooo@gmail.com>
2022-01-23 12:48:48 -0700

    Improved visual appearance, removed old images, and reduced size of problematic coding image

---
+++ main.js
@@ -0,0 +1,14 @@
+//let contact = document.getElementById("contact_icon");
+
+// hover_state = () => {
+//     contact.style.backgroundColor = 'white';
+//     contact.style.color = 'black'
+// }
+
+// hover_out = () => {
+//     contact.style.backgroundColor = 'black';
+//     contact.style.color = 'white';
+// }
+
+// contact.addEventListener('mouseover',hover_state);
+// contact.addEventListener('mouseout',hover_out);
